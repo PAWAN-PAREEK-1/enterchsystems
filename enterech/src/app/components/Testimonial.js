4d334f1aec2778e4c842e8b386b7d09e3efad9aa
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-09-26 17:55:26 +0530

    testimonial update

--- enterech/src/app/components/Testimonial.js
+++ enterech/src/app/components/Testimonial.js
@@ -1,56 +1,66 @@
-import React from "react";
-import Link from "next/link";
+"use client"
+import React, { useState } from "react";
 import Image from "next/image";
 import styles from "../style/Testimonial.module.css";
 import testibg from "../img/testibg.svg";
 import testi from "../img/testi.svg";
 import testiicon from "../img/testiicon.svg";
 
+const testimonialsData = [
+  {
+    imageSrc: testi,
+    iconSrc: testiicon,
+    title: "profession, Reliable & COST Effective",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean facilisis aliquet odio, sit amet rutrum magna. Duis cursus leo in condimentum cursus. Etiam vitae interdum ex, suscipit aliquam erat. Donec lacinia imperdiet mi, ut mattis sem pulvinar a. Praesent placerat, velit at luctus pulvinar, dolor est faucibus orci, vel posuere tellus enim non elit",
+    author: "Emery Culhane",
+  },
+  {
+    imageSrc: testi,
+    iconSrc: testiicon,
+    title: "Title 2",
+    text: "Testimonial text 2...",
+    author: "Author 2",
+  },
+
+];
+
 const Testimonial = () => {
+  const [selectedTestimonial, setSelectedTestimonial] = useState(0);
+
+  const handleCircleClick = (index) => {
+    setSelectedTestimonial(index);
+  };
+
   return (
     <section className={styles.indexTesti}>
       <div className={styles.up}>
-
-        <div className={styles.indexInfo}>
-        <div className={styles.info}>
-        <span>Our Statistics</span>
-          <h1>Some Important Facts</h1>
-        </div>
-        <div className={`${styles.info} ${styles.info2}`}>
-          <h1>5000+</h1>
-         <h4>Residential Projects</h4>
-        </div>
-        <div className={`${styles.info} ${styles.info2}`}>
-          <h1>1000+</h1>
-          <h4>Commercial Projects</h4>
-        </div>
-        <div className={`${styles.info} ${styles.info2}`}>
-          <h1>15000+</h1>
-          <h4>Industrial Projects</h4>
-        </div>
-        </div>
+        {/* Your statistics section */}
       </div>
       <div className={styles.down} style={{ textAlign: 'right' }}>
-        <Image src={testibg}/>
-
+        <Image src={testibg} />
 
         <div className={styles.testimonial}>
-                <div className={styles.testiImg}>
-                        <Image src={testi}/>
-                        <Image src={testiicon} id={styles.testiicon}/>
-
-                </div>
-                <div className={styles.detail}>
-                            <span>What Our Clients Say</span>
-                            <h2>profession, Reliable & COST Effective</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean facilisis aliquet odio, sit amet rutrum magna. Duis cursus leo in condimentum cursus. Etiam vitae interdum ex, suscipit aliquam erat. Donec lacinia imperdiet mi, ut mattis sem pulvinar a. Praesent placerat, velit at luctus pulvinar, dolor est faucibus orci, vel posuere tellus enim non elit</p>
-                            <h4><hr />Emery Culhane</h4>
-                            <div className={styles.circel}>
-                                <span></span>
-                            </div>
-                </div>
-
+          <div className={styles.testiImg}>
+            <Image src={testimonialsData[selectedTestimonial].imageSrc} />
+            <Image src={testimonialsData[selectedTestimonial].iconSrc} id={styles.testiicon} />
+          </div>
+          <div className={styles.detail}>
+            <span>What Our Clients Say</span>
+            <h2>{testimonialsData[selectedTestimonial].title}</h2>
+            <p>{testimonialsData[selectedTestimonial].text}</p>
+            <h4><hr />{testimonialsData[selectedTestimonial].author}</h4>
+            <div className={styles.circles}>
+          {testimonialsData.map((_, index) => (
+            <span
+              key={index}
+              className={`${styles.circle} ${selectedTestimonial === index ? styles.activeCircle : ''}`}
+              onClick={() => handleCircleClick(index)}
+            ></span>
+          ))}
         </div>
+          </div>
+        </div>
+
 
       </div>
     </section>
@@ -58,3 +68,6 @@ const Testimonial = () => {
 };
 
 export default Testimonial;
+
+
+
