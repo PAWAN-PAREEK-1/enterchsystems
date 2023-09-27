5a16ecc5c710072ba643629d3886060b535e9e1c
PAWAN-PAREEK-1 <pawapareek506@gmail.com>
2023-09-27 12:15:33 +0530

    bloog section

--- enterech/src/app/page.js
+++ enterech/src/app/page.js
@@ -7,6 +7,7 @@ import About from "@/app/components/About";
 import Product from "@/app/components/Product";
 import Testimonial from "@/app/components/Testimonial";
 import Recents from "@/app/components/Recents";
+import Blog from "@/app/components/Blog";
 import styles from "@/app/style/Index.module.css";
 import "../app/globals.css";
 import sline from "./img/sline.svg";
@@ -82,6 +83,7 @@ export default function Home() {
       <Product/>
       <Testimonial/>
       <Recents/>
+      <Blog/>
     </main>
   );
 }
